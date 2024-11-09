34dd6c11bcf7d7e26b2b79f9d0fe01527d1e67c3
markus <modj62361@edu.ucl.dk>
2024-11-10 00:11:21 +0100

    listbox - tenancies

--- EksamensProjekt/TenancyView.xaml.cs
+++ EksamensProjekt/TenancyView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +21,47 @@ namespace EksamensProjekt
     /// </summary>
     public partial class TenancyView : Window
     {
+        public ObservableCollection<Lejemål> LejemålList { get; set; }
+        public ObservableCollection<string> TenantList { get; set; }
         public TenancyView()
         {
             InitializeComponent();
+            LejemålList = new ObservableCollection<Lejemål>
+            {
+                new Lejemål { Address = "Skovløbervej 11, 2400 København NV", Rooms = 3, Size = 85, Tenant = "Anders Jensen" },
+                new Lejemål { Address = "Bogtrykkervej 32, 2400 København NV", Rooms = 2, Size = 65, Tenant = "Sofie Hansen" },
+                new Lejemål { Address = "Vollsmosse 13H, 5240 Odense C", Rooms = 4, Size = 100, Tenant = "Thomas Nielsen" },
+                new Lejemål { Address = "Hybovej 5A, 9440 Aabybro", Rooms = 5, Size = 120, Tenant = "Marie Petersen" }
+            };
+            TenantList = new ObservableCollection<string>
+            {
+                "Anders Jensen",
+                "Sofie Hansen",
+                "Thomas Nielsen",
+                "Marie Petersen",
+                "Frederik Larsen",
+                "Camilla Sørensen"
+            };
+
+            DataContext = this;
+            AddressListBox.ItemsSource = LejemålList;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ændringer er gemt!");
+            // Her kunne du evt. implementere yderligere logik for at opdatere lejemålsdataene permanent (fx i en database)
         }
 
        
     }
+    public class Lejemål
+    {
+        public string Address { get; set; }
+        public int Rooms { get; set; }
+        public int Size { get; set; }
+        public string Tenant { get; set; }
+    }
+
+
 }
