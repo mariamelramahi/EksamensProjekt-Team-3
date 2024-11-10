ff30fcc7486a0392f55d2a74ee4ff584093e6b16
Marlen Halvorsen <maha63312@edu.ucl.dk>
2024-11-10 07:59:18 +0100

    Added service file and classes

---
+++ EksamensProjekt/Services/TenancyService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensProjekt.Services
+{
+    public class TenancyService
+    {
+        public IRepo<Tenancy> tenancyRepo;
+        public IRepo<Tenant> tenantRepo;
+
+
+
+        public void CreateNewTenancy()
+        {
+
+        }
+
+
+    }
+}
