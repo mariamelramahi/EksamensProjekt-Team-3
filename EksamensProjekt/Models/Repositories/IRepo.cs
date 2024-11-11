7a82932caa514c1c14748355e3ce342dedeb3c13
Mariam El <mariamchokolade@gmail.com>
2024-11-11 22:22:26 +0100

    Classes

---
+++ EksamensProjekt/Models/Repositories/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensProjekt.Models.Repositories
+{
+    public interface IRepo<T>
+    {
+        T GetByID(int id);
+        void Create(T entity);
+        IEnumerable<T> ReadAll();
+        void Update(T entity);
+        void Delete(int entity);
+    }
+}
