ae98d0ccc056334c210a26c58a26839d65d987f7
Marlen Halvorsen <maha63312@edu.ucl.dk>
2024-11-13 09:47:10 +0100

    bug fix

--- EksamensProjekt/Model/ModifiedExcelAddress.cs
+++ EksamensProjekt/Model/ModifiedExcelAddress.cs
@@ -9,13 +9,6 @@ namespace EksamensProjekt.Model
 {
     public class ModifiedExcelAddress : Address
     {
-        public int AddressID { get; set; }
-        public string StreetName { get; set; }
-        public string Number { get; set; }
-        public string Floor { get; set; }
-        public string ZipCode { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
         public Status? TenancyStatus { get; set; }
         public DateTime? MoveInDate { get; set; }
         public DateTime? MoveOutDate { get; set; }
--- EksamensProjekt/Model/StandardAddress.cs
+++ EksamensProjekt/Model/StandardAddress.cs
@@ -8,13 +8,6 @@ namespace EksamensProjekt.Model
 {
     public class StandardAddress : Address
     {
-        public int AddressID {  get; set; }
-        public string StreetName { get; set; }
-        public string Number { get; set; }
-        public string Floor { get; set; }
-        public string ZipCode { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
         public StandardAddress()
         {
         }
