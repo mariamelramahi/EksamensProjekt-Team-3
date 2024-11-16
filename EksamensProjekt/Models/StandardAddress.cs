a6e7b0680c83ed6ff86d3744495165e7765966d3
markus <modj62361@edu.ucl.dk>
2024-11-16 15:45:30 +0100

    listbox fixx

--- EksamensProjekt/Models/StandardAddress.cs
+++ EksamensProjekt/Models/StandardAddress.cs
@@ -14,5 +14,10 @@ namespace EksamensProjekt.Models
         public string FloorNumber { get; set; }
         public string Zipcode { get; set; }
         public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return Street + " " + Number + ", " + FloorNumber + ", " + Zipcode + ", " + Country;
+        }
     }
 }
