615c3afe45d2db0068feac45db861fd2c786b295
markus <modj62361@edu.ucl.dk>
2024-11-17 14:45:59 +0100

    Util namespace og BooleanToStringConverter class

---
+++ EksamensProjekt/Utilities/BooleanToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EksamensProjekt.Utilities
+{
+    public class BooleanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool booleanValue)
+            {
+                return booleanValue ? "Ja" : "Nej";
+            }
+            return "Nej"; // Hvis værdien er null
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string stringValue)
+            {
+                if (stringValue == "Ja")
+                    return true;
+                if (stringValue == "Nej")
+                    return false;
+            }
+            return false; // Default værdi
+        }
+    }
+
+}
