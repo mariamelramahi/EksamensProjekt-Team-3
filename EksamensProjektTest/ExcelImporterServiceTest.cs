f8a8e817e1179d04e54af8a8d2585973cf6c51b5
Marlen Halvorsen <maha63312@edu.ucl.dk>
2024-11-27 21:21:47 +0100

    Added Unittest for ExcelImporter Service. Test written for failure, as method do not ignore header in XLS. Expected to pass when header is ignored.

--- EksamensProjektTest/ExcelImporterServiceTest.cs
+++ EksamensProjektTest/ExcelImporterServiceTest.cs
@@ -1,11 +1,5 @@
 ﻿using EksamensProjekt.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EksamensProjekt.Models;
-using Moq;
 
 namespace EksamensProjektTest
 {
@@ -24,13 +18,36 @@ namespace EksamensProjektTest
 
 
         [TestMethod]
+        [DeploymentItem("Resources/TestAddresses.xlsx", "Resources")]
+
         public void UploadAddresses_ValidFiles_ReturnsAddressList() 
         {
             //ARRANGE
+            var filePath = Path.Combine("Resources", "TestAddresses.xlsx");
+            //var fullPath = Path.GetFullPath(filePath);
+            //var fullPath = @"C:\Users\marle\Desktop\Datamatiker anden semester\ValhalConnect\EksamensProjektTest";
 
             //ACT
-            List<Address> addresses = excelImporterService.ImportAddresses();
+            List<Address> addresses = excelImporterService.ImportAddresses(filePath);
+            
             //ASSERT
+            Assert.AreEqual(addresses.Count, 3);
+            //testing if address objects are equal using equals override
+            Assert.AreEqual(addresses[0], CreateAddress("Main Street", "123", "1A", "1000", "Denmark"));
+            Assert.AreEqual(addresses[1], CreateAddress("Second Street", "456", "2B", "2000", "Sweden"));
+            Assert.AreEqual(addresses[2], CreateAddress("Third Avenue", "789", "3C", "3000", "Norway"));
         }
+        private Address CreateAddress(string street, string number, string floor, string zipcode, string country)
+        {
+            return new Address()
+            {
+                Street = street,
+                Number = number,
+                FloorNumber = floor,
+                Zipcode = zipcode,
+                Country = country
+            };
+        }
+
     }
 }
