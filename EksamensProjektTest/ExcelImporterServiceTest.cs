41138a886ef160cafd8f7bf93cdd5bd8f01c126d
Marlen Halvorsen <maha63312@edu.ucl.dk>
2024-11-27 20:03:38 +0100

    Test

---
+++ EksamensProjektTest/ExcelImporterServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using EksamensProjekt.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EksamensProjekt.Models;
+using Moq;
+
+namespace EksamensProjektTest
+{
+
+    [TestClass]
+    public class ExcelImporterServiceTest
+    {
+        ExcelImportService excelImporterService;
+
+        
+        [TestInitialize]
+        public void TestInitialize() 
+        {
+            excelImporterService = new ExcelImportService();
+        }
+
+
+        [TestMethod]
+        public void UploadAddresses_ValidFiles_ReturnsAddressList() 
+        {
+            //ARRANGE
+
+            //ACT
+            List<Address> addresses = excelImporterService.ImportAddresses();
+            //ASSERT
+        }
+    }
+}
