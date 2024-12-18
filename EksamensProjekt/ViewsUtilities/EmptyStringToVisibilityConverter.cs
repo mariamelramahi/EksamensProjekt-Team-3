c2a2de29e59f19b66962834a95a4ff076d03fa90
Kenneth Haagen Sørensen <khso63309@edu.ucl.dk>
2024-12-18 10:10:17 +0100

    Chore: Moved Utilities used by View to its own folder

--- EksamensProjekt/ViewsUtilities/EmptyStringToVisibilityConverter.cs
+++ EksamensProjekt/ViewsUtilities/EmptyStringToVisibilityConverter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
-namespace EksamensProjekt.Utilities
+namespace EksamensProjekt.ViewsUtilities
 {
     public class EmptyStringToVisibilityConverter : IValueConverter
     {
